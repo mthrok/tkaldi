4e09f329ee65dab7ee1403e28cb6b1f814e9d357
moto <855818+mthrok@users.noreply.github.com>
2020-11-25 21:20:22 +0000

    Update matrix/vector

--- src/libtkaldi/src/matrix/kaldi-matrix.h
+++ src/libtkaldi/src/matrix/kaldi-matrix.h
@@ -21,8 +21,6 @@ void assert_matrix_shape(const torch::Tensor &tensor_) {
 
 } // namespace
 
-template<typename Real> struct VectorBase;
-
 // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L44-L48
 template<typename Real>
 struct MatrixBase {
@@ -47,6 +45,94 @@ struct MatrixBase {
   // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L177-L178
   void CopyColFromVec(const VectorBase<Real> &v, const MatrixIndexT col) {
     tensor_.index_put_({Slice(), Slice(col)}, v.tensor_);
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L99-L107
+  inline Real&  operator() (MatrixIndexT r, MatrixIndexT c) {
+    // CPU only
+    return tensor_.accessor<Real, 2>()[r][c];
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L112-L120
+  inline const Real operator() (MatrixIndexT r, MatrixIndexT c) const {
+    return tensor_.index({Slice(r), Slice(c)}).item().to<Real>();
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L138-L141
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.cc#L859-L898
+  template<typename OtherReal>
+  void CopyFromMat(const MatrixBase<OtherReal> & M,
+                   MatrixTransposeType trans = kNoTrans) {
+    auto src = M.tensor_;
+    if (trans == kTrans)
+      src = src.transpose(1, 0);
+    tensor_.index_put_({Slice(), Slice()}, src);
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L186-L191
+  inline const SubVector<Real> Row(MatrixIndexT i) const {
+    return SubVector<Real>(*this, i);
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L208-L211
+  inline SubMatrix<Real> RowRange(const MatrixIndexT row_offset,
+                                  const MatrixIndexT num_rows) const {
+    return SubMatrix<Real>(*this, row_offset, num_rows, 0, NumCols());
+  }
+
+protected:
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L749-L753
+  explicit MatrixBase(): tensor_(torch::empty({0, 0})) {
+    KALDI_ASSERT_IS_FLOATING_TYPE(Real);
+  }
+};
+
+// https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L781-L784
+template<typename Real>
+struct Matrix : MatrixBase<Real> {
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L786-L787
+  Matrix() : MatrixBase<Real>() {}
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L789-L793
+ Matrix(const MatrixIndexT r, const MatrixIndexT c,
+        MatrixResizeType resize_type = kSetZero,
+        MatrixStrideType stride_type = kDefaultStride):
+      MatrixBase<Real>() { Resize(r, c, resize_type, stride_type); }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L859-L874
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.cc#L817-L857
+  void Resize(const MatrixIndexT r,
+              const MatrixIndexT c,
+              MatrixResizeType resize_type = kSetZero,
+              MatrixStrideType stride_type = kDefaultStride) {
+    // Note: The original Kaldi implementation uses in-place resizing, but here, a new Tensor is allocated.
+    // TODO: Check the difference.
+    auto &tensor_ = MatrixBase<Real>::tensor_;
+    switch(resize_type) {
+    case kSetZero:
+      tensor_ = torch::zeros({r, c});
+      break;
+    case kUndefined:
+      tensor_ = torch::empty({r, c});
+      break;
+    case kCopyData:
+      auto t = torch::zeros({r, c});
+      auto num_row = r < tensor_.size(0) ? r : tensor_.size(0);
+      auto num_col = c < tensor_.size(1) ? c : tensor_.size(1);
+      t.index_put_({Slice(None, num_row), Slice(None, num_col)}, tensor_.index({Slice(None, num_row), Slice(None, num_col)}));
+      tensor_ = t;
+      break;
+    }
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-matrix.h#L876-L883
+  Matrix<Real> &operator = (const MatrixBase<Real> &other) {
+    if (MatrixBase<Real>::NumRows() != other.NumRows() ||
+        MatrixBase<Real>::NumCols() != other.NumCols())
+      Resize(other.NumRows(), other.NumCols(), kUndefined);
+    MatrixBase<Real>::CopyFromMat(other);
+    return *this;
   }
 };
 
--- src/libtkaldi/src/matrix/kaldi-vector.h
+++ src/libtkaldi/src/matrix/kaldi-vector.h
@@ -39,13 +39,20 @@ struct VectorBase {
   ////////////////////////////////////////////////////////////////////////////////
   // Kaldi-compatible methods
   ////////////////////////////////////////////////////////////////////////////////
-  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L362-L365
-  explicit VectorBase() : tensor_(torch::empty({0})) {
-    assert_vector_shape<Real>(tensor_);
-  };
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L42-L43
+  void SetZero() { Set(0); }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L48-L49
+  void Set(Real f) { tensor_.index_put_({"..."}, f); }
 
   // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L62-L63
   inline MatrixIndexT Dim() const { return tensor_.numel(); };
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L68-L69
+  inline Real* Data() { return tensor_.data_ptr<Real>(); }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L71-L72
+  inline const Real* Data() const { return tensor_.data_ptr<Real>(); }
 
   // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L74-L79
   inline Real operator() (MatrixIndexT i) const {
@@ -58,22 +65,28 @@ struct VectorBase {
     return tensor_.accessor<Real, 1>()[i];
   };
 
-  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L444-L451
-  void Resize(MatrixIndexT length, MatrixResizeType resize_type = kSetZero) {
-    switch(resize_type) {
-    case kSetZero:
-      tensor_ = torch::zeros({length});
-      break;
-    case kUndefined:
-      tensor_ = torch::empty({length});
-      break;
-    case kCopyData:
-      auto t = torch::zeros({length});
-      auto numel = length > tensor_.numel() ? tensor_.numel() : length;
-      t.index_put_({Slice(None, numel)}, tensor_.index({Slice(None, numel)}));
-      tensor_ = t;
-      break;
-    }
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L88-L95
+  SubVector<Real> Range(const MatrixIndexT o, const MatrixIndexT l) {
+    return SubVector<Real>(*this, o, l);
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L97-L105
+  const SubVector<Real> Range(const MatrixIndexT o,
+                              const MatrixIndexT l) const {
+    return SubVector<Real>(*this, o, l);
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L107-L108
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.cc#L226-L233
+  void CopyFromVec(const VectorBase<Real> &v) {
+    TORCH_INTERNAL_ASSERT(tensor_.sizes() == v.tensor_.sizes());
+    tensor_.copy_(v.tensor_);
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L181-L184
+  template<typename OtherReal>
+  void AddVec(const Real alpha, const VectorBase<OtherReal> &v) {
+    tensor_ += alpha * v.tensor_;
   }
 
   // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L196-L198
@@ -86,6 +99,44 @@ struct VectorBase {
     }
     tensor_.addmv(mat, v.tensor_, beta, alpha);
   }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L233-L234
+  void Add(Real c) { tensor_ += c; }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L236-L239
+  void AddVecVec(Real alpha, const VectorBase<Real> &v,
+                 const VectorBase<Real> &r, Real beta) {
+    tensor_ = beta * tensor_ + alpha * v.tensor_ * r.tensor_;
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L246-L247
+  void Scale(Real alpha) { tensor_ *= alpha; }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L305-L306
+  Real Min() const {
+    if (tensor_.numel()) {
+      return tensor_.min().item().to<Real>();
+    }
+    return std::numeric_limits<Real>::infinity();
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L308-L310
+  Real Min(MatrixIndexT *index) const {
+    TORCH_INTERNAL_ASSERT(tensor_.numel());
+    torch::Tensor value, ind;
+    std::tie(value, ind) = tensor_.min(0);
+    *index = ind.item().to<MatrixIndexT>();
+    return value.item().to<Real>();
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L312-L313
+  Real Sum() const { return tensor_.sum().item().to<Real>(); };
+
+protected:
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L362-L365
+  explicit VectorBase() : tensor_(torch::empty({0})) {
+    assert_vector_shape<Real>(tensor_);
+  };
 };
 
 // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L385-L390
@@ -106,15 +157,62 @@ struct Vector : VectorBase<Real> {
   // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L395-L399
   explicit Vector(const MatrixIndexT s,
                   MatrixResizeType resize_type = kSetZero)
-      : VectorBase<Real>() {  VectorBase<Real>::Resize(s, resize_type);  }
+      : VectorBase<Real>() {  Resize(s, resize_type);  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L412-L416
+  explicit Vector(const VectorBase<Real> &v) : VectorBase<Real>() {
+    Resize(v.Dim(), kUndefined);
+    this->CopyFromVec(v);
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L434-L435
+  void Swap(Vector<Real> *other) {
+    auto tmp = VectorBase<Real>::tensor_;
+    this->tensor_ = other->tensor_;
+    other->tensor_ = tmp;
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L444-L451
+  void Resize(MatrixIndexT length, MatrixResizeType resize_type = kSetZero) {
+    // Note: The original Kaldi implementation uses in-place resizing, but here, a new Tensor is allocated.
+    // TODO: Check the difference.
+    auto &tensor_ = VectorBase<Real>::tensor_;
+    switch(resize_type) {
+    case kSetZero:
+      tensor_ = torch::zeros({length});
+      break;
+    case kUndefined:
+      tensor_ = torch::empty({length});
+      break;
+    case kCopyData:
+      auto t = torch::zeros({length});
+      auto numel = length > tensor_.numel() ? tensor_.numel() : length;
+      t.index_put_({Slice(None, numel)}, tensor_.index({Slice(None, numel)}));
+      tensor_ = t;
+      break;
+    }
+  }
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L463-L468
+  Vector<Real> &operator = (const VectorBase<Real> &other) {
+    Resize(other.Dim(), kUndefined);
+    this->CopyFromVec(other);
+    return *this;
+  }
 };
 
 // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L482-L485
 template<typename Real>
 struct SubVector : VectorBase<Real> {
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L487-L499
   SubVector(const VectorBase<Real> &t, const MatrixIndexT origin,
             const MatrixIndexT length)
     : VectorBase<Real>(t.tensor_.index({Slice(origin, origin + length)}))
+    {}
+
+  // https://github.com/kaldi-asr/kaldi/blob/7fb716aa0f56480af31514c7e362db5c9f787fd4/src/matrix/kaldi-vector.h#L524-L528
+  SubVector(const MatrixBase<Real> &matrix, MatrixIndexT row)
+    : VectorBase<Real>(matrix.tensor_.index({Slice(row), Slice()}))
     {}
 };
 
