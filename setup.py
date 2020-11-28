18ad6c7a3199ae995879ea4fe2586d6a8628e7c0
moto <855818+mthrok@users.noreply.github.com>
2020-11-27 22:01:54 -0500

    Use CMake for build process (#11)

--- setup.py
+++ setup.py
@@ -1,29 +1,82 @@
 import os
-import setuptools
+import subprocess
 from pathlib import Path
 
-from torch.utils.cpp_extension import CppExtension, BuildExtension
+import torch
+import setuptools
+from setuptools.command.build_ext import build_ext
 
 _ROOT_DIR = Path(__file__).parent.resolve()
 _CSRC_DIR = _ROOT_DIR / 'src' / 'libtkaldi'
 
 
-def _get_srcs():
-    return [str(p) for p in _CSRC_DIR.glob('**/*.cc')]
+def _get_cxx11_abi():
+    try:
+        return int(torch._C._GLIBCXX_USE_CXX11_ABI)
+    except ImportError:
+        return 0
 
 
-def _get_include_dirs():
-    return [
-        str(_CSRC_DIR / 'src'),
-    ]
+class BuildExtension(build_ext):
+    def build_extension(self, ext):
+        extdir = os.path.abspath(
+            os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+
+        cfg = "Debug" if self.debug else "Release"
+
+        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
+        # from Python.
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+            f"-DCMAKE_BUILD_TYPE={cfg}",
+            f"-DCMAKE_PREFIX_PATH={torch.utils.cmake_prefix_path}",
+            f"-DCMAKE_CXX_FLAGS=-D_GLIBCXX_USE_CXX11_ABI={_get_cxx11_abi()}",
+        ]
+        build_args = [
+            "--verbose",
+        ]
+
+        # default to Ninja
+        if 'CMAKE_GENERATOR' not in os.environ:
+            cmake_args += ["-GNinja"]
+
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
+            if hasattr(self, "parallel") and self.parallel:
+                # CMake 3.12+ only.
+                build_args += ["-j{}".format(self.parallel)]
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        subprocess.check_call(
+            ["cmake", str(_ROOT_DIR)] + cmake_args, cwd=self.build_temp
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
+
+    def get_ext_filename(self, fullname):
+        ext_filename = super().get_ext_filename(fullname)
+        ext_filename_parts = ext_filename.split('.')
+        without_abi = ext_filename_parts[:-2] + ext_filename_parts[-1:]
+        ext_filename = '.'.join(without_abi)
+        return ext_filename
 
 
 def _get_ext_modules():
     return [
-        CppExtension(
-            name='tkaldi._tkaldi',
-            sources=_get_srcs(),
-            include_dirs=_get_include_dirs(),
+        setuptools.Extension(
+            name='tkaldi.libtkaldi',
+            sources=[],
         ),
     ]
 
@@ -34,9 +87,7 @@ def _main():
         version="0.0.1",
         description="Experimental project to compile Kaldi with Torch.",
         ext_modules=_get_ext_modules(),
-        cmdclass={
-            'build_ext': BuildExtension.with_options(no_python_abi_suffix=True)
-        },
+        cmdclass={'build_ext': BuildExtension},
         packages=setuptools.find_packages(where='src'),
         package_dir={'': 'src'},
         install_requires=[
